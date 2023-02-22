2ef088cfdf97407ead16e67e9f4c8a294a03f170
Devin Sackett <dsac@hotmail.com>
2023-02-22 15:08:37 -0800

    Publish a file to a given post endpoint

--- main.ts
+++ main.ts
@@ -1,86 +1,73 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import axios from "axios";
+import * as E from "fp-ts/lib/Either";
+import { identity, pipe } from "fp-ts/lib/function";
+import * as O from "fp-ts/lib/Option";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as fs from "fs";
+import {
+	App,
+	FileSystemAdapter,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+import * as path from "path";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface Settings {
+	url: string;
+	apiKey: string;
+	apiSecret: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: Settings = {
+	url: "",
+	apiKey: "",
+	apiSecret: "",
+};
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: Settings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		const adapter = this.app.vault.adapter as FileSystemAdapter;
+		const basePath = adapter.getBasePath();
+		const { url, apiKey, apiSecret } = this.settings;
+		const token = Buffer.from(`${apiKey}:${apiSecret}`).toString("base64");
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu, file) => {
+				if (!("extension" in file)) {
+					return;
 				}
-			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+				menu.addItem((item) => {
+					item
+						.setTitle("Publish file")
+						.setIcon("document")
+						.onClick(() =>
+							pipe(
+								path.join(basePath, file.path),
+								log("Publishing file..."),
+								TE.fromNullable(new Error("File not found")),
+								TE.chain(publishFile(url, token)),
+								TE.chain(moveFile),
+								TE.match(
+									(e) => notify(e, "File failed to publish"),
+									() => notify(undefined, "File has been published")
+								)
+							)()
+						);
+				});
+			})
+		);
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new BlogPublisherTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -91,23 +78,82 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+const buildFormData = (data: Record<string, any>) => {
+	const form = new FormData();
+
+	Object.entries(data).forEach(([key, value]) => form.append(key, value));
+
+	return form;
+};
+
+const publishFile = (url: string, token: string) => (filePath: string) =>
+	pipe(
+		{ fileName: path.parse(filePath).name, file: fs.readFileSync(filePath) },
+		buildFormData,
+		TE.tryCatchK(
+			(data) =>
+				axios.post(url, data, {
+					headers: {
+						"Content-Type": "multipart/form-data",
+						Authorization: "Basic " + token,
+					},
+				}),
+			E.toError
+		),
+		TE.chain(() => TE.right(filePath))
+	);
+
+const createDir = (dir: string) =>
+	pipe(
+		dir,
+		E.fromPredicate(
+			(dir) => !fs.existsSync(dir),
+			() => new Error("directory exists")
+		),
+		E.chain((dir) => E.tryCatch(() => fs.mkdirSync(dir), E.toError)),
+		E.match(
+			() => dir,
+			() => dir
+		)
+	);
+
+const log =
+	(msg: string) =>
+	<A>(a: A) => {
+		console.log(msg);
+		return a;
+	};
+
+const moveFile = (filePath: string) =>
+	pipe(
+		path.parse(filePath),
+		O.fromPredicate(({ dir }) => !dir.contains("published")),
+		O.let("file", () => filePath),
+		O.let("newPath", ({ dir }) => createDir(path.join(dir, "published"))),
+		O.map(({ file, base, newPath }) =>
+			pipe(
+				E.tryCatch(
+					() => fs.renameSync(file, path.join(newPath, base)),
+					E.toError
+				),
+				E.map(() => newPath)
+			)
+		),
+		O.match(() => E.right<Error, string>(""), identity),
+		TE.fromEither
+	);
+
+const notify = (e: Error | undefined, msg: string) => {
+	console.log(msg);
+
+	if (e) {
+		console.error(e);
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+	new Notice(msg);
+};
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class BlogPublisherTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -116,22 +162,55 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h3", { text: "Blog Publisher Settings" });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Publisher url")
+			.setDesc(
+				"This should be a POST url where the file is sent as a multipart/form-data body to."
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter the url")
+					.setValue(this.plugin.settings.url)
+					.onChange(async (value) => {
+						this.plugin.settings.url = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("API Key")
+			.setDesc(
+				"API Key used when posting a blog to the URL. NOTE: this is passed as the user of the basic authorization header."
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your key")
+					.setValue(this.plugin.settings.apiKey)
+					.onChange(async (value) => {
+						this.plugin.settings.apiKey = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("API Secret")
+			.setDesc(
+				"API Secret used when posting a blog to the URL. NOTE: this is passed as the password of the basic authorization header."
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.apiSecret)
+					.onChange(async (value) => {
+						this.plugin.settings.apiSecret = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
